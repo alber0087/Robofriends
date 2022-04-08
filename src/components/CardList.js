fdd2132360d6b86eb3becb538ad4396a97703205
albertorg0087 <albertorg0087@gmail.com>
2022-04-08 12:25:12 +0100

    Implementing Hooks

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,23 +2,23 @@ import React from "react";
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    return (
-        <div>
-            {
-                robots.map((user, i) => {
-                    return (
-                        <Card 
-                            key={i} 
-                            id={robots[i].id} 
-                            name={robots[i].name} 
-                            email={robots[i].email}
-                        />  
-                    );
+  return (
+    <div>
+      {
+        robots.map((user, i) => {
+          return (
+            <Card 
+              key={i} 
+              id={robots[i].id} 
+              name={robots[i].name} 
+              email={robots[i].email}
+              />  
+          );
 
-                })
-            }
-        </div>
-    );
+        })
+      }
+    </div>
+  );
 }
 
 export default CardList;           
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,12 +3,12 @@ import React from "react";
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className="pa2">
-          <input 
-            className="pa3 ba b--green bg-lightest-blue"
-            type='search' 
-            placeholder="search robots" 
-            onChange={searchChange}
-          />  
+      <input 
+        className="pa3 ba b--green bg-lightest-blue"
+        type='search' 
+        placeholder="search robots" 
+        onChange={searchChange}
+        />  
     </div>
   );
 }
